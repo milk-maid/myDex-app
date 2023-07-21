04bc45036861e5aa0d967b83f05cb3581e2c35d3
milk-maid <roqbell@gmail.com>
2023-07-21 11:03:05 +0100

    moving   on

--- hardhat/scripts/dexDeploy.js
+++ hardhat/scripts/dexDeploy.js
@@ -39,3 +39,13 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// Token deployed to: 0x7BB9785845321937F1f67FeB5283Ab589Ce31E5e
+// Exchange deployed to: 0x60D73331E7be701ed0730f440a44E0998E754713
+
+// Token deployed to: 0xF3C921919E61bCbe25F54c04076d9Bd92F98fa22
+// Exchange deployed to: 0x0EAAdfB85DACC299a4D77d80Caf422e3Ea17831c
+
+// Token deployed to: 0x918Ac529a70c092B61008d60FADAc3c251B23388
+// Exchange deployed to: 0x27d51ADDDd96458B76423cEDed8e9F80131b0192
